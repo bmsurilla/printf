3e5f66943af82da92f0a641bf37b38c3da6fdcbc
bmsurilla <bm.surilla@gmail.com>
2026-06-17 01:38:15 +0200

    ft_format

--- ft_printf.c
+++ ft_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsurilla <bsurilla@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bea_s <bea_s@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/05 01:21:06 by bea_s             #+#    #+#             */
-/*   Updated: 2026/06/15 20:58:09 by bsurilla         ###   ########.fr       */
+/*   Updated: 2026/06/17 01:28:39 by bea_s            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,64 +26,47 @@ int	ft_putstr(char *str)
 	return (i);
 }
 
-int		ft_format(const char *str, va_list format, int i)
-{
-	va_start (format, str);
-	i = 0;
+int		ft_format(char formatter, va_list format)
+{	
+	int	i;
 	
-	while(str[i])
+	i = 0;
+	if (formatter == "c")
+		ft_putchar(va_arg(format, char));
+	// else if (str[i] == "s")
+	// 	str[i] = ft_putstr(va_arg(format, char*));
+	else if (formatter == "p")
+		i = i + ft_putptr(va_arg(format, void*));
+	else if (formatter == "d" || formatter == "i")
+		i = i + ft_putnbr (va_arg(format, int));
+	else if (formatter == "u")
+		i = i + ft_putnbr_u(va_arg(format, unsigned int))
+	else if (formatter == "x")
+		i = i + ft_putnbr_hex(va_arg(format, unsigned long), HEX_LOWER);
+	else if (formatter == "X")
+		i = i + ft_putnbr_hex(va_arg(format, unsigned long), HEX_UPPER);
+	else (formatter) == "%")
+		i = i + ft_putchar(%);
+	return (i);
+	}
+}
+
+int		ft_printf(const char *str, ...)
+{
+	va_start	(format, *str);
+	int			i;
+	
+	i = 0;
+	while (str[i])
 	{
-		if(str[i] == "%")
+		if (str[i] = "%")
 		{
 			i++;
-			if (str[i] == "c")
-			{
-				i++;
-				ft_putchar(va_arg(format, char c));
-				// return (i + ft_putchar(*str));
-			}
-			
-			
-			else if (str[i] == "p")
-			{
-				i++;
-				ft_putptr(*str);
-				return (i + ft_putptr(*str));
-			}
-			else if (str[i] == "d" || str[i] == "i")
-			{
-				i++;
-				ft_putnbr (*str);
-				return (i + putnbr (*str));
-			}
-			else if (str[i] == "u")
-			{
-				i++;
-				ft_putnbr_u(*str)
-				return ( i + ft_putnbr_u)
-			}
-			else if (str[i] == "x")
-			{
-				i++;
-				ft_putnbr_hex(*str, HEX_LOWER);
-				return (i + ft_putnbr_hex(*str, HEX_LOWER));
-			}
-			else if (str[i] == "X")
-			{
-				i++;
-				ft_putnbr_hex(*str, HEX_UPPER);
-				return (i + ft_putnbr_hex(*str, HEX_UPPER));
-			}
-			else if (str[i] == "%")
-			{
-				i++;
-				ft_putchar(%);
-				return (i + ft_putchar(%));
-			}
-			else
-			ft_putstr(*str);
-			i++;
-			return (i);
+			ft_format(str[i], format);
 		}
+		else
+			ft_putstr(*str);
+			i++;		
 	}
+	return (i);
 }
